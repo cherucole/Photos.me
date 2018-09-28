9c87a455243190de8a54be3698450f4212913284
cherucole <cheruyotcheruyot@gmail.com>
2018-09-28 17:22:10 +0300

    heroku

--- photos_project/settings.py
+++ photos_project/settings.py
@@ -83,6 +83,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'photos_project.urls'
